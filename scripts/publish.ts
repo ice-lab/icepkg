d0c009fbde78b61d819ccbccf6eadfea35bc743b
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-08-03 17:53:00 +0800

    chore: publish ci

---
+++ scripts/publish.ts
@@ -0,0 +1,46 @@
+/**
+ * Scripts to check unpublished version and run publish
+ */
+import { join } from 'path';
+import { spawnSync } from 'child_process';
+import { IPackageInfo, getPackageInfos } from './getPackageInfos';
+
+if (process.env.BRANCH_NAME !== 'master') {
+  console.log('No Publish', process.env.BRANCH_NAME);
+  return;
+}
+
+function publish(pkg: string, version: string, directory: string): void {
+  console.log('[PUBLISH]', `${pkg}@${version}`);
+
+  spawnSync('npm', [
+    'publish',
+    // use default registry
+  ], {
+    stdio: 'inherit',
+    cwd: directory,
+  });
+}
+
+// Entry
+console.log('[PUBLISH] Start:');
+
+Promise.all([
+  getPackageInfos(join(__dirname, '../packages'))
+]).then((result: IPackageInfo[][]) => {
+
+  let publishedCount = 0;
+  // Publish
+  for (let i = 0; i < result.length; i++) {
+    const packageInfos: IPackageInfo[] = result[i];
+    for (let j = 0; j < packageInfos.length; j++) {
+      const { name, directory, localVersion, shouldPublish } = packageInfos[j];
+      if (shouldPublish) {
+        publishedCount++;
+        console.log(`--- ${name}@${localVersion} ---`);
+        publish(name, localVersion, directory);
+      }
+    }
+  }
+  console.log(`[PUBLISH] Complete (count=${publishedCount}).`)
+});
