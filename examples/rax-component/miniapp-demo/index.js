919c5a0105bdd1dab71626d11c08e929386480e1
ClarkXia <xiawenwu41@gmail.com>
2020-10-15 12:28:30 +0800

    fix: miniapp demo

---
+++ examples/rax-component/miniapp-demo/index.js
@@ -0,0 +1,8 @@
+import { createElement } from 'rax';
+import MyComponent from 'example-component';
+
+function App(){
+  return <MyComponent />;
+}
+
+export default App;
--- packages/build-plugin-component/src/configs/rax/miniapp/getBase.js
+++ packages/build-plugin-component/src/configs/rax/miniapp/getBase.js
@@ -1,15 +1,32 @@
+const path = require('path');
+const fse = require('fs-extra');
+const glob = require('glob');
 const { setComponentConfig } = require('miniapp-compile-config');
-
 const getWebpackBase = require('../getBaseWebpack');
 const getOutputPath = require('./getOutputPath');
+const { MINIAPP } = require('../../../constants');
 
+const parseTarget = (target) => target === MINIAPP ? 'ali-miniapp' : target;
 module.exports = (context, target, options = {}, onGetWebpackConfig) => {
-  const { entryPath = './src/index', distDir = '' } = options[target] || {};
+  const { rootDir, command } = context;
+  const { distDir = '' } = options[target] || {};
   const outputPath = getOutputPath(context, { target, distDir });
   const config = getWebpackBase(context, {
     disableRegenerator: true
   });
-
+  let entryPath = './src/index';
+  if (command === 'start') {
+    const miniappDemoFolder = 'miniapp-demo';
+    const filesPath = glob.sync('index.*', { cwd: path.join(rootDir, miniappDemoFolder) });
+    filesPath.forEach((filePath) => {
+      entryPath = `./${miniappDemoFolder}/${filePath}`;
+    });
+  }
+  // copy template file to .miniapp
+  const miniappOutput = path.join(rootDir, '.miniapp', parseTarget(target));
+  fse.ensureDirSync(miniappOutput);
+  fse.copySync(path.join(__dirname, `../../../template/miniapp/${parseTarget(target)}`), miniappOutput);
+  
   setComponentConfig(config, options[target], {
     onGetWebpackConfig,
     context,
