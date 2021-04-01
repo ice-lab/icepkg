396b610599ff8fb9b2836e822b7829bbc7727419
chen <class4cxy@126.com>
2021-04-01 20:05:37 +0800

    Update dev.js
    feat: add htmlInjection for web

--- packages/build-plugin-component/src/configs/rax/web/dev.js
+++ packages/build-plugin-component/src/configs/rax/web/dev.js
@@ -3,34 +3,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { hmrClient } = require('rax-compile-config');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const setCSSRule = require('../../../utils/setCSSRule');
+const defaultConfig = require('./defaultConfig.js');
+const { configHTMLPlugin, configHTMLContent, configWebpack } = require('../../../utils/htmlInjection');
 
 module.exports = (config, context, options) => {
   const { entries } = options;
+  const { userConfig } = context;
 
-  Object.keys(entries).forEach((entryKey) => {
+  // set default content of html
+  configHTMLContent(defaultConfig);
+
+  // config htmlInjection for once
+  if (userConfig.htmlInjection) {
+    configWebpack(userConfig.htmlInjection);
+  }
+
+  const entrieKeys = Object.keys(entries);
+
+  entrieKeys.forEach((entryKey) => {
     config
       .entry(entryKey)
       .add(hmrClient)
       .add(entries[entryKey]);
 
-    config.plugin(`html4${entryKey}`).use(HtmlWebpackPlugin, [
+    config.plugin(entrieKeys > 1 ? `HtmlWebpackPlugin_${entryKey}` : `HtmlWebpackPlugin`).use(HtmlWebpackPlugin, [
       {
         inject: false,
         filename: entryKey,
         chunks: [entryKey],
-        templateParameters: {
-          htmlAttrs: '',
-          headPrepend: [
-            '<meta charset="UTF-8">',
-            '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
-            '<meta http-equiv="X-UA-Compatible" content="ie=edge">',
-          ].join(''),
-          headAppend: ['<title>DEMO 预览</title>'],
-          rootContainer: '<div id="root"></div>',
-          bodyPrepend: '',
-          bodyAppend: '',
-          bodyAttrs: '',
-        },
         template: path.resolve(__dirname, '../../../template/demo.html'),
       },
     ]);
