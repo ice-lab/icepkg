95228b9acd69a8436310d843a8e503d8492443fb
chen <class4cxy@126.com>
2021-04-06 16:26:50 +0800

    增加build指令下的html定制能力

--- packages/build-plugin-component/src/configs/rax/getDemoConfig.js
+++ packages/build-plugin-component/src/configs/rax/getDemoConfig.js
@@ -5,34 +5,40 @@ const { hmrClient } = require('rax-compile-config');
 const getBaseWebpack = require('./getBaseWebpack');
 const setCSSRule = require('../../utils/setCSSRule');
 const { getRaxDemoEntryJs } = require('../../utils/handlePaths');
+const defaultConfig = require('./web/defaultConfig.js');
+const { configHTMLPlugin, configHTMLContent, configWebpack } = require('../../utils/htmlInjection');
 
 module.exports = (context, options) => {
-  const { command, rootDir } = context;
+  const { command, rootDir, userConfig } = context;
   const { demos, entries, inlineStyle = true } = options;
   const config = getBaseWebpack(context, { ...options, name: 'demo' });
   const portalPath = getRaxDemoEntryJs(rootDir);
-  if (command === 'start') {
-    config
-      .entry('portal')
-      .add(hmrClient)
-      .add(portalPath);
-  } else {
-    config.entry('portal').add(portalPath);
-  }
 
   config.output.filename('[name].js');
   config.output.publicPath('./');
   config.output.path(path.join(rootDir, 'build'));
   setCSSRule(config, inlineStyle);
+
+  // set default content of html
+  configHTMLContent(defaultConfig);
+
+  // config htmlInjection for once
+  if (userConfig.htmlInjection) {
+    configWebpack(userConfig.htmlInjection);
+  }
+
   if (command === 'start') {
     config.output.publicPath('/demo');
   } else {
-    Object.keys(entries).forEach((entryKey) => {
+    const entrieKeys = Object.keys(entries);
+
+    entrieKeys.forEach((entryKey) => {
       config
         .entry(`demo/${entryKey}`)
         .add(entries[entryKey]);
 
-      config.plugin(`html4${entryKey}`).use(HtmlWebpackPlugin, [
+      // config.plugin(`html4${entryKey}`).use(HtmlWebpackPlugin, [
+      config.plugin(entrieKeys.length > 1 ? `HtmlWebpackPlugin_demo/${entryKey}` : `HtmlWebpackPlugin`).use(HtmlWebpackPlugin, [
         {
           inject: true,
           filename: `demo/${entryKey}.html`,
@@ -44,6 +50,23 @@ module.exports = (context, options) => {
         },
       ]);
     });
+
+    // custom html config
+    configHTMLPlugin(config);
+
+    /**
+     * 后移的原因是entries数量会受portal的影响
+     * 导致htmlInjection插件名生成与上面不一致
+     */
+    if (command === 'start') {
+      config
+        .entry('portal')
+        .add(hmrClient)
+        .add(portalPath);
+    } else {
+      config.entry('portal').add(portalPath);
+    }
+    
     config.plugin('minicss').use(MiniCssExtractPlugin, [
       {
         filename: '[name].css',
