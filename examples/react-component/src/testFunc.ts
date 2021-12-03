36b5c19bd90e6068060a80482e43d6489516cfe3
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2021-12-03 15:51:42 +0800

    fix: handle react-docgen parse error

---
+++ examples/react-component/src/testFunc.ts
@@ -0,0 +1,9 @@
+/**
+ * This is a test function
+ */
+export function testFunc(
+  /** Number input value */
+  num: number,
+) {
+  return Boolean(num % 2);
+}
--- packages/build-plugin-component/src/utils/getReactDocs.js
+++ packages/build-plugin-component/src/utils/getReactDocs.js
@@ -7,7 +7,7 @@ const camelcase = require('camelcase');
 const reactDocgen = require('react-docgen');
 const formatPathForWin = require('./formatPathForWin');
 
-module.exports = function getReactComponents(srcPath) {
+module.exports = function getReactDocs(srcPath) {
   if (!existsSync(srcPath)) {
     return [];
   }
@@ -18,19 +18,24 @@ module.exports = function getReactComponents(srcPath) {
       const filePath = formatPathForWin(join(srcPath, filename));
       const content = readFileSync(filePath, 'utf-8');
 
-      const reactDoc = reactDocgen.parse(content, null, null, {
-        filename: filePath,
-      });
+      try {
+        const reactDoc = reactDocgen.parse(content, null, null, {
+          filename: filePath,
+        });
 
-      filename = filename.replace(/\.[tj]sx?$/, '');
-      const href = `/?demo=${filename}`;
+        filename = filename.replace(/\.[tj]sx?$/, '');
+        const href = `/?demo=${filename}`;
 
-      return {
-        href,
-        filename,
-        pascalCaseName: camelcase(filename, { pascalCase: true }),
-        filePath,
-        reactDoc,
-      };
-    });
+        return {
+          href,
+          filename,
+          pascalCaseName: camelcase(filename, { pascalCase: true }),
+          filePath,
+          reactDoc,
+        };
+      } catch (e) {
+        return null;
+      }
+    })
+    .filter(Boolean);
 };
