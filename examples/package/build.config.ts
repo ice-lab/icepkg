d8a8878cda30042a99a22ef9c3b8481478ae0667
maoxiaoke <maoxiaoke@outlook.com>
2022-04-21 17:21:28 +0800

    fix: 🐛 resolve load hooks params

--- examples/package/build.config.ts
+++ examples/package/build.config.ts
@@ -14,11 +14,12 @@ export default defineConfig({
     },
   },
   transform: {
+    formats: ['es2017'],
     excludes: '**/test/*',
   },
-  bundle: {
-    development: true,
-    formats: ['esm', 'es2017'],
-    externals: true,
-  },
+  // bundle: {
+  //   development: true,
+  //   formats: ['esm', 'es2017'],
+  //   externals: true,
+  // },
 });
