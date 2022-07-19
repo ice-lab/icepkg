be916b3947e12389f616de4aeda7569bdc3b4a7e
逆葵 <xianyong.yxy@alibaba-inc.com>
2022-07-19 15:00:47 +0800

    feat(create-pkg): remove prompt of scope choose in ali Intranet

--- packages/create-pkg/src/langs/zh-CN.ts
+++ packages/create-pkg/src/langs/zh-CN.ts
@@ -3,7 +3,6 @@ export default {
   version: '@ice/create-pkg 版本： ',
   dirExistFiles: '当前目录已存在文件，是否继续？',
   initSuccess: '项目初始化成功',
-  generateIntranetComponent: '生成仅内网可用的包',
   selectNpmScope: '请选择 npm scope',
   packageName: '包名',
   packageNameValidateError: (name: string) => `npm 包名 ${name} 校验不通过，请重试`,
