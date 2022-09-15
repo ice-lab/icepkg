51ac596cf03326a8b84a4e6cd4f2f3de8ce56102
changchangge <1173484831@qq.com>
2022-09-15 19:11:51 +0800

    feat: add some tips

--- packages/build-plugin-component/src/utils/mergeBabelPlugins.js
+++ packages/build-plugin-component/src/utils/mergeBabelPlugins.js
@@ -1,5 +1,6 @@
 module.exports = (defaultPlugins, babelPlugins) => {
   const pluginsMap = {};
+  // 把插件名作为key，pluginsMap不存在key则新增插件，存在则覆盖插件
   const appendNewPluginsToMap = item => {
     if (typeof item === 'string') {
       pluginsMap[item] = item;
@@ -8,7 +9,9 @@ module.exports = (defaultPlugins, babelPlugins) => {
       pluginsMap[key] = item
     }
   }
+  // 默认插件存入pluginsMap
   defaultPlugins.map(appendNewPluginsToMap);
+  // 业务自定义插件存入pluginsMap
   babelPlugins.map(appendNewPluginsToMap);
   return Object.values(pluginsMap);
 }
