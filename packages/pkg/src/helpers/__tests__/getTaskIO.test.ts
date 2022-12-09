d33b859992303a3c897a38da3ce8bb987a4bb094
ZeroLing <zhuoling.lcl@alibaba-inc.com>
2022-12-09 17:13:35 +0800

    fix: test case

--- packages/pkg/src/helpers/__tests__/getTaskIO.test.ts
+++ packages/pkg/src/helpers/__tests__/getTaskIO.test.ts
@@ -2,11 +2,11 @@ import { test, expect } from 'vitest';
 import { formatEntry } from '../getTaskIO';
 
 test('formatEntry', () => {
-  expect(formatEntry('src/index.ts')).toEqual({
+  expect(formatEntry('src/index')).toEqual({
     index: 'src/index',
   });
 
-  expect(formatEntry(['src/index.ts', 'src/client.ts'])).toEqual({
+  expect(formatEntry(['src/index', 'src/client'])).toEqual({
     index: 'src/index',
     client: 'src/client',
   });
