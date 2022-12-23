44f57161f4eaf1b80fe32a0b26a8b52ab4bbae14
luhc228 <luhengchang228@126.com>
2022-12-23 20:44:07 +0800

    chore: update type

--- packages/pkg/types.d.ts
+++ packages/pkg/types.d.ts
@@ -35,111 +35,15 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
-declare module '*.ico' {
-  const src: string;
-  export default src;
-}
 declare module '*.webp' {
   const src: string;
   export default src;
 }
-declare module '*.avif' {
-  const src: string;
-  export default src;
-}
 
-// media
-declare module '*.mp4' {
-  const src: string;
-  export default src;
-}
-declare module '*.webm' {
-  const src: string;
-  export default src;
-}
-declare module '*.ogg' {
-  const src: string;
-  export default src;
-}
-declare module '*.mp3' {
-  const src: string;
-  export default src;
-}
-declare module '*.wav' {
-  const src: string;
-  export default src;
-}
-declare module '*.flac' {
-  const src: string;
-  export default src;
-}
-declare module '*.aac' {
-  const src: string;
-  export default src;
-}
-
-// fonts
-declare module '*.woff' {
-  const src: string;
-  export default src;
-}
-declare module '*.woff2' {
-  const src: string;
-  export default src;
-}
-declare module '*.eot' {
-  const src: string;
-  export default src;
-}
-declare module '*.ttf' {
-  const src: string;
-  export default src;
-}
-declare module '*.otf' {
-  const src: string;
-  export default src;
-}
-
-// web worker
-declare module '*?worker' {
-  const workerConstructor: {
-    new(): Worker;
-  };
-  export default workerConstructor;
-}
-declare module '*?worker&inline' {
-  const workerConstructor: {
-    new(): Worker;
-  };
-  export default workerConstructor;
-}
-declare module '*?sharedworker' {
-  const sharedWorkerConstructor: {
-    new(): SharedWorker;
-  };
-  export default sharedWorkerConstructor;
-}
-
-// other
-declare module '*.pdf' {
-  const src: string;
-  export default src;
-}
-declare module '*.txt' {
-  const src: string;
-  export default src;
-}
-declare module '*?url' {
-  const src: string;
-  export default src;
-}
-declare module '*?raw' {
-  const src: string;
-  export default src;
-}
-
+// global env
 declare let __DEV__: boolean;
 
+// process.env
 declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
