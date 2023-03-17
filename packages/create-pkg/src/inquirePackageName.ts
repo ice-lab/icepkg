ee04a919808bcfc4d0bc51366525f0b9a4db82d3
luhc228 <luhengchang228@126.com>
2023-03-17 18:50:40 +0800

    feat: support generate sub package

--- packages/create-pkg/src/inquirePackageName.ts
+++ packages/create-pkg/src/inquirePackageName.ts
@@ -5,14 +5,14 @@ import getInfo from './langs/index.js';
 
 export async function inquirePackageName() {
   const info = await getInfo();
-  const isIntranet = await checkAliInternal();
+  const isInternal = await checkAliInternal();
 
   const { npmName } = await inquirer.prompt([
     {
       type: 'input',
       name: 'npmName',
       message: info.packageName,
-      default: `${isIntranet ? '@ali/' : ''}example-component`,
+      default: `${isInternal ? '@ali/' : ''}example-component`,
       validate: (value) => {
         if (!validateName(value).validForNewPackages) {
           return info.packageNameValidateError(value);
