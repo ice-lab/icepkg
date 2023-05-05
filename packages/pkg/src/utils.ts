975b29d918618bd4634329908bf460afdd9d92b1
Homyee King <HomyeeKing@gmail.com>
2023-05-05 14:00:13 +0800

    feat: up swc/core (#532)
    * feat: up swc/core
    
    * feat: check version to emit error msg
    
    * feat: add docs
    
    * chore: bump
    
    * fix: cmt
    
    * fix: cmt

--- packages/pkg/src/utils.ts
+++ packages/pkg/src/utils.ts
@@ -334,6 +334,7 @@ export function checkDependencyExists(dependency: string, link: string) {
     consola.error(`当前组件/库依赖 \`${dependency}\`, 请运行命令 \`npm i ${dependency} --save\` 安装此依赖。更多详情请看 \`${link}\``);
     process.exit(1);
   }
+  return pkg?.dependencies?.[dependency];
 }
 
 export const getBuiltInPlugins = () => [
