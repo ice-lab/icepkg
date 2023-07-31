5c2b3139cb88c7e65df79741e20e473a2a62b44d
cici.jmy <cici.jmy@koubei.com>
2023-07-31 10:05:40 +0800

    feat: 保持队形

--- packages/plugin-docusaurus/src/remark/fixedFilename.js
+++ packages/plugin-docusaurus/src/remark/fixedFilename.js
@@ -3,7 +3,7 @@ const DEMO_PREFIX = 'IcePkgDemo';
 /** Use docPath */
 const fixedFilename = (filepath, rootDir) => {
   try {
-    const componentName = filepath.replaceAll(rootDir, '').replace(/\.mdx?$/, '');
+    const componentName = filepath.replace(rootDir, '').replace(/\.mdx?$/, '');
     return `${DEMO_PREFIX}${componentName}`.replace(/[/\\-]/g, '_');
   } catch (e) {
     return '';
