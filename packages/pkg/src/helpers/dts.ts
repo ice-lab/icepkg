0309863824f1b4799149e8ef6ef4f6bbeef77d83
Homyee King <HomyeeKing@gmail.com>
2023-09-12 14:28:28 +0800

    fix: dts maybe failed (#589)
    * fix: dts maybe failed
    
    * chore: change to official npm registry
    
    * chore: rm npmrc
    
    * chore: changeset

--- packages/pkg/src/helpers/dts.ts
+++ packages/pkg/src/helpers/dts.ts
@@ -136,7 +136,7 @@ export async function dtsCompile({ files, alias, rootDir, outputDir }: DtsCompil
 
   const result = _files.map((file) => ({
     ...file,
-    dtsContent: runFile({ fileContents: dtsFiles[file.dtsPath], filePath: file.dtsPath }),
+    dtsContent: dtsFiles[file.dtsPath] ? runFile({ fileContents: dtsFiles[file.dtsPath], filePath: file.dtsPath }) : '',
   }));
 
   logger.debug(`Generating declaration files take ${timeFrom(dtsCompileStart)}`);
