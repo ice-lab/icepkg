339fee2e186dfe2d9e2cc6f969cba4b3ab6f6996
luhc228 <luhengchang228@126.com>
2023-10-23 10:15:38 +0800

    fix: not compile non src entry (#615)
    * fix: not compile non src entry
    
    * chore: changeset
    
    * feat: add test

--- packages/pkg/src/utils.ts
+++ packages/pkg/src/utils.ts
@@ -336,9 +336,11 @@ export const createScriptsFilter = (
   extraIncludes: RegExp[] = [],
   extraExcludes: RegExp[] = [],
 ) => {
-  const includes = [/src\/.*\.(?:[cm]?[jt]s|[jt]sx)$/].concat(extraIncludes);
-  const excludes = [/\.d\.ts$/, /core-js/, /core-js-pure/, /tslib/, /@swc\/helpers/, /@babel\/runtime/, /babel-runtime/].concat(extraExcludes);
+  // Match non node_modules files. In ICEPKG V2, we only compile src/**.m?[jt]sx? files.
+  const includes = [/^(?!.*node_modules\/).*\.(?:[cm]?[jt]s|[jt]sx)$/].concat(extraIncludes);
 
+  const notCompiledDeps = ['core-js', 'core-js-pure', 'tslib', '@swc/helpers', '@babel/runtime', 'babel-runtime'];
+  const excludes = [/\.d\.ts$/, new RegExp(`node_modules/(${notCompiledDeps.join('|')})`)].concat(extraExcludes);
   return createFilter(includes, excludes);
 };
 
