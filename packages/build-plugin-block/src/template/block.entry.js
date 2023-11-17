7771bec221ad6bcb05cd21d2e5198bcf1db426a1
luhc228 <luhengchang228@126.com>
2023-11-17 12:08:24 +0800

    chore: add comment

--- packages/build-plugin-block/src/template/block.entry.js
+++ packages/build-plugin-block/src/template/block.entry.js
@@ -6,4 +6,5 @@ import ReactDOM from 'react-dom';
 import Block from '@/block';
 
 const mountNode = document.querySelector('#mountNode');
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(<Block />, mountNode);
