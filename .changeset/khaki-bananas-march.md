03c68384f7771bd95d79249fda9b737a6cc85386
XGHeaven <xgheaven@gmail.com>
2024-08-19 20:16:38 +0800

    feat: support browser option for bundle

---
+++ .changeset/khaki-bananas-march.md
@@ -0,0 +1,5 @@
+---
+'@ice/pkg': minor
+---
+
+support `browser` option for bundle mode #650
---
+++ packages/pkg/tests/projects/bundle-browser.test.ts
@@ -0,0 +1,20 @@
+import { runProjectTest } from "./helper";
+
+runProjectTest('bundle-browser', [{
+  name: 'default',
+  config: {
+    transform: { formats: [] },
+    bundle: {
+      formats: ['esm', 'cjs']
+    }
+  }
+}, {
+  name: 'enable-browser',
+  config: {
+    transform: { formats: [] },
+    bundle: {
+      formats: ['esm', 'cjs'],
+      browser: true
+    }
+  }
+}])
