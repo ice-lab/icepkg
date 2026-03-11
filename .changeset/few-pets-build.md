4da01eaa967151544130a8afe3db1b4605b9505b
XGHeaven <xgheaven@gmail.com>
2026-03-11 15:17:40 +0800

    feat: improve cjs import and performance (#706)

---
+++ .changeset/few-pets-build.md
@@ -0,0 +1,5 @@
+---
+'@ice/pkg': minor
+---
+
+feat: improve cjs import and performance
--- tests/integration/mix-cjs-mjs/src/entry-mts.ts
+++ tests/integration/mix-cjs-mjs/src/entry-mts.ts
@@ -1,6 +1,8 @@
 import { add, extend } from './cjs';
+import { ctsFile } from './cts-file.cjs';
 
 export { add, extend };
+export { ctsFile };
 
 export const foo: Record<string, any> = {
   ...extend({ a: 1 }, { b: 2 }),
